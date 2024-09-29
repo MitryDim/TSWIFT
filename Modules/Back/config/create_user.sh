f6518851bb92388602fea6e31a5b22e6e177947c
MitryDim <dimcorb@gmail.com>
2024-09-29 22:30:12 +0200

    Ajout de la gestion des mots de passe pour les utilisateurs MaxScale, mise à jour des fichiers de configuration et création de nouveaux scripts SQL pour la gestion des utilisateurs. Intégration des dépendances nécessaires dans package.json et amélioration de la génération de configuration.

---
+++ Modules/Back/config/create_user.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Utiliser les variables d'environnement injectées par CDKTF
+mariadb -uroot -p${MYSQL_ROOT_PASSWORD} <<-EOSQL
+CREATE USER IF NOT EXISTS 'maxscale_admin'@'%' IDENTIFIED BY '${MAXSCALE_ADMIN_PASSWORD}';
+CREATE USER IF NOT EXISTS '${MYSQL_DB_USER}'@'%' IDENTIFIED BY '${MYSQL_DB_USER_PASSWORD}';
+GRANT SELECT ON mysql.user TO 'maxscale_admin'@'%';
+GRANT SELECT ON mysql.db TO 'maxscale_admin'@'%';
+GRANT SELECT ON mysql.tables_priv TO 'maxscale_admin'@'%';
+GRANT SELECT ON mysql.columns_priv TO 'maxscale_admin'@'%';
+GRANT SELECT ON mysql.procs_priv TO 'maxscale_admin'@'%';
+GRANT SELECT ON mysql.proxies_priv TO 'maxscale_admin'@'%';
+GRANT SELECT ON mysql.roles_mapping TO 'maxscale_admin'@'%';
+GRANT SHOW DATABASES ON *.* TO 'maxscale_admin'@'%';
+FLUSH PRIVILEGES;
+
+CREATE USER IF NOT EXISTS 'maxscale_monitor'@'%' IDENTIFIED BY '${MAXSCALE_MONITORING_PASSWORD}';
+
+GRANT REPLICATION CLIENT on *.* to 'maxscale_monitor'@'%';
+GRANT REPLICATION SLAVE on *.* to 'maxscale_monitor'@'%';
+GRANT SLAVE MONITOR ON *.* TO 'maxscale_monitor'@'%';
+GRANT SUPER, RELOAD on *.* to 'maxscale_monitor'@'%';
+GRANT READ_ONLY ADMIN ON *.* TO 'maxscale_monitor'@'%';
+GRANT BINLOG ADMIN ON *.* TO 'maxscale_monitor'@'%';
+GRANT REPLICATION SLAVE ADMIN ON *.* TO 'maxscale_monitor'@'%';
+FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOSQL
--- Modules/Back/main.ts
+++ Modules/Back/main.ts
@@ -8,6 +8,8 @@ import { mkdirSync, writeFileSync } from "fs";
 import { generateMaxScaleConfig } from "../../generateConfig";
 import { dirname, join } from "path";
 import path = require("path");
+import * as dotenv from "dotenv";
+dotenv.config();
 
 interface BackProps {
   network: Network;
@@ -46,13 +48,18 @@ export class Back extends Construct {
       keepLocally: true,
     });
 
-    const initSQL = path.resolve(__dirname, "./config/init.sql");
+    const sqlConfig = path.resolve(__dirname, "./config/create_user.sh");
+
     this.maxscaleContainer = new Container(this, "dbContainer", {
       name: `db-master-${props.envConfig.name}`,
       image: dbImage.name,
       env: [
-        `MYSQL_ROOT_PASSWORD=${props.variables.rootPassword}`,
-        `MYSQL_DATABASE=${props.variables.dbName}`
+        `MYSQL_ROOT_PASSWORD=${props.variables.rootPassword.value}`,
+        `MYSQL_DB_USER_PASSWORD=${props.variables.dbPassword.value}`,
+        `MYSQL_DATABASE=${props.variables.dbName.value}`,
+        `MAXSCALE_MONITORING_PASSWORD=${props.variables.maxscaleMonitorPassword.value}`,
+        `MAXSCALE_ADMIN_PASSWORD=${props.variables.maxscaleAdminPassword.value}`,
+        `MYSQL_DB_USER=${props.variables.dbUser.value}`,
       ],
       networksAdvanced: [
         {
@@ -65,8 +72,8 @@ export class Back extends Construct {
           containerPath: "/var/lib/mysql",
         },
         {
-          hostPath: initSQL, // Emplacement local du fichier SQL
-          containerPath: "/docker-entrypoint-initdb.d/init.sql", // Chemin à l'intérieur du conteneur
+          hostPath: sqlConfig, // Emplacement local du fichier SQL
+          containerPath: "/docker-entrypoint-initdb.d/create_user.sh", // Chemin à l'intérieur du conteneur
         },
       ],
       ports: [
@@ -79,7 +86,7 @@ export class Back extends Construct {
         "--log-bin=mysql-bin",
         "--binlog-format=row",
         "--gtid-domain-id=1",
-        "--log-slave-updates=1"
+        "--log-slave-updates=1",
       ],
     });
 
@@ -91,8 +98,12 @@ export class Back extends Construct {
         name: `db-slave-${props.envConfig.name}${i}`,
         image: dbImage.name,
         env: [
-          `MYSQL_ROOT_PASSWORD=${props.variables.rootPassword}`,
-          `MYSQL_DATABASE=${props.variables.dbName}`
+          `MYSQL_ROOT_PASSWORD=${props.variables.rootPassword.value}`,
+          `MYSQL_DB_USER_PASSWORD=${props.variables.dbPassword.value}`,
+          `MYSQL_DATABASE=${props.variables.dbName.value}`,
+          `MAXSCALE_MONITORING_PASSWORD=${props.variables.maxscaleMonitorPassword.value}`,
+          `MAXSCALE_ADMIN_PASSWORD=${props.variables.maxscaleAdminPassword.value}`,
+          `MYSQL_DB_USER=${props.variables.dbUser.value}`,
         ],
         volumes: [
           {
@@ -100,8 +111,8 @@ export class Back extends Construct {
             containerPath: "/var/lib/mysql",
           },
           {
-            hostPath: initSQL, // Emplacement local du fichier SQL
-            containerPath: "/docker-entrypoint-initdb.d/init.sql", // Chemin à l'intérieur du conteneur
+            hostPath: sqlConfig, // Emplacement local du fichier SQL
+            containerPath: "/docker-entrypoint-initdb.d/create_user.sh", // Chemin à l'intérieur du conteneur
           },
         ],
         networksAdvanced: [
@@ -135,15 +146,17 @@ export class Back extends Construct {
         `db-slave-${props.envConfig.name}3`,
       ],
       maxscaleUser: "maxscale_admin",
-      maxscalePassword: "secret",
     });
 
     writeFileSync(configPath, config);
 
-   new Container(this, "maxscaleContainer", {
+    new Container(this, "maxscaleContainer", {
       name: "maxscale",
       image: maxscaleImage.name,
-      env: ["MAXSCALE_USER=maxscale_admin", "MAXSCALE_PASSWORD=secret"],
+      env: [
+        "MAXSCALE_USER=maxscale_admin",
+        `MAXSCALE_PASSWORD=${props.variables.maxscaleAdminPassword.value}`,
+      ],
       networksAdvanced: [
         {
           name: props.network.name,
